8b685c4ea2a78cb72a1ab88763120fd643500435
ljj <yeshrb@126.com>
2018-04-18 09:01:58 +0800

    complete

--- main/main.js
+++ main/main.js
@@ -1,5 +1,42 @@
 // Write your cade below:
-function main() {};
+function main() {
+    return {
+        guessed:createGuessedNumber(),
+        guess(guessed = this.guessed,guess) {
+            guess += '';
+            if(guessed.length !== guess.length) {
+                throw new Error("parameters must have the same length");
+            }
+            let hitsA = 0;
+            let hitsB = 0;
+            for(let i=0; i< guess.length;i++){
+                if(guessed.indexOf(guess.charAt(i)) === i){
+                    hitsA ++;
+                }
+                else if (guessed.indexOf(guess.charAt(i)) > -1) {
+                    hitsB ++;
+                }
+            }
+            return `${hitsA}A${hitsB}B`;
+        }
+    }
+};
+function createGuessedNumber(){
+    let guessed = '';
+    for(let i =0 ; i< 4;i++){
+        let num = getRandomIntInclusive(1,9);
+        while(guessed.indexOf(num) > -1) {
+            num = getRandomIntInclusive(1,9);
+        }
+        guessed += num;
+    }
+    return guessed.trim();
+}
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
 
 module.exports = {
     main
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,63 @@
-const main = require('../main/main');
+const main = require('../main/main').main;
 
 describe('main()', () => {
+
+    it('should return 4A0B given the number is 1234 when you guess 1234',()=>{
+        let guessed = '1234';
+        let guess = '1234';
+        expect(main().guess(guessed,guess)).toBe('4A0B');
+    })
+    it('should return 0A4B given number is 1234 when you guess 4321',()=> {
+        let guessed = '1234';
+        let guess = '4321';
+        expect(main().guess(guessed,guess)).toBe('0A4B');
+    })
+    it('should return 1A3B given number is 1234 when you guess 4321',()=> {
+        let guessed = '1234';
+        let guess = '1567';
+        expect(main().guess(guessed,guess)).toBe('1A0B');
+    })
+    it('should return 2A2B given number is 1234 when you guess 1324',()=> {
+        let guessed = '1234';
+        let guess = '1324';
+        expect(main().guess(guessed,guess)).toBe('2A2B');
+    })
+    it('should return 0A0B given number is 1234 when you guess 5678',()=> {
+        let guessed = '1234';
+        let guess = '5678';
+        expect(main().guess(guessed,guess)).toBe('0A0B');
+    })
+
+
+    it('should return 4A0B given a random number when you guess the same number',()=> {
+        let guessed = main().guessed;
+        let guess = guessed;
+        expect(main().guess(guessed,guess)).toBe('4A0B');
+    })
+    it('should return 0A4B given a random number when you guess the reverse number',()=> {
+        let guessed = main().guessed;
+        let guess = guessed.split('').reverse().join('');
+
+        expect(main().guess(guessed,parseInt(guess))).toBe('0A4B');
+    })
+    it('should return 2A2B given a random number when you guess the the number that first and last same and  swap 2nd and 3rd ',()=> {
+        let guessed = main().guessed;
+        let arr = guessed.split('');
+        let temp = arr[1];
+        arr[1] = arr[2];
+        arr[2] = temp;
+        let guess = arr.join('');
+        expect(main().guess(guessed,parseInt(guess))).toBe('2A2B');
+    })
+    it('should return a random 4-digits number not contains duplicated number',()=> {
+        let guessed = main().guessed;
+        let guessedArr = guessed.split('');
+        let reduceArr = guessedArr.reduce((accu,curr) => {
+            (accu.indexOf(curr) > -1)? accu:accu.push(curr);
+            return accu;
+        },[]);
+        expect(reduceArr.length).toBe(4);
+
+    })
 
 });
